d3f65f3fa4cb29815553f40aacb579b35980be56
Baha-Abdelatif <baha.abdelatif@gmail.com>
2021-08-03 23:46:27 +0200

    Initial commit, npx create-strapi-app

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed39611a4748ae21ee050e3096b17831'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b39052a4-b4f5-4cbe-880e-6cc147b43e7f'
+};
